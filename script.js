fbacf3366517aecff50b80fcc1243ae8eb58c564
Fionna <fionnagemzon@gmail.com>
2020-12-14 19:52:40 -0600

    image

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   let inputs = document.getElementById('faultyItems');
+   let userInputs = document.getElementById('faultyItems');
    form.addEventListener("submit", function(event) {
        let pilotNameInput = document.querySelector("input[name=pilotName]").value;
        let copilotNameInput = document.querySelector("input[name=copilotName]").value;
        let fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
        let cargoMassInput = document.querySelector("input[name=cargoMass]").value;
-       if (pilotNameInput === "" || copilotNameInput === "" || isNaN(fuelLevelInput) || isNaN(cargoMassInput) ) {
+       if (!isNaN(pilotNameInput) || !isNaN(copilotNameInput) || isNaN(fuelLevelInput) || isNaN(cargoMassInput)) {
           alert("Make sure to enter valid information for each field!");
-          inputs.style.visibility = 'hidden';
-          
-       }else{
-          inputs.style.visibility = 'visible';
-          document.getElementById('pilotStatus').innerHTML=`Pilot ${pilotNameInput}`;
-          document.getElementById('coPilotStatus').innerHTML=`Co-Pilot ${copilotNameInput}`;
-          
-         event.preventDefault();
-       }
+          event.preventDefault();
+       } 
+      document.getElementById('pilotStatus').innerHTML=`Pilot ${pilotNameInput}`;
+      document.getElementById('copilotStatus').innerHTML=`Co-Pilot ${copilotNameInput}`;
+
+         if(fuelLevelInput<10000){
+            document.getElementById('faultyItems').style.visibility='visible';
+            document.getElementById("fuelStatus").innerHTML=`Warning: Fuel too low for launch`
+            document.getElementById('launchStatus').innerHTML=`Shuttle not ready for launch`;
+            document.getElementById('launchStatus').style.color = "red";
+            event.preventDefault();
+         } 
+         
+         if(cargoMassInput>10000){
+            document.getElementById("cargoStatus").innerHTML=`Warning: Cargo mass too high for launch`
+            document.getElementById('launchStatus').innerHTML=`Shuttle not ready for launch`;
+            document.getElementById('launchStatus').style.color = "red";
+            event.preventDefault();
+         }
+
+         if(fuelLevelInput>10000 && cargoMassInput<10000){
+            document.getElementById('launchStatus').innerHTML=`Shuttle is ready for launch`;
+            document.getElementById('launchStatus').style.color = "green"
+            event.preventDefault();
+         }
+
   });  
 });
 
@@ -27,10 +44,10 @@ window.addEventListener("load", function() {
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${Mars}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+   <li>Diameter: ${6779 km}</li>
+   <li>Star: ${Sol}</li>
+   <li>Distance from Earth: ${225 million km from Earth}</li>
+   <li>Number of Moons: ${2}</li>
 </ol>
-<img src="${}">
+<img src="${https://mars.nasa.gov/system/resources/detail_files/7808_global-color-views-mars-PIA00407-full2.jpg}">
 */
